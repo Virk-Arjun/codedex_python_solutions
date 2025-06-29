bff742fc97fa666e127391764bc0289eadddb9b8
arjun virk <virkvarjun@gmail.com>
2025-06-28 22:18:15 -0400

    Create d_r_y.py

---
+++ ch6/d_r_y.py
@@ -0,0 +1,9 @@
+print("Hello from dry.py!")
+
+name = "Python"
+print("Length of name:", len(name))
+
+print("Type of name:", type(name))
+
+quotient, remainder = divmod(13, 5)
+print("divmod(13, 5):", (quotient, remainder))
