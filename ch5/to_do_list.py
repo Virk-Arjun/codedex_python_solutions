60db01cb7f17c1bce03d67efb7a89f9a7c13facc
arjun virk <virkvarjun@gmail.com>
2025-06-28 22:14:31 -0400

    Create to_do_list.py

---
+++ ch5/to_do_list.py
@@ -0,0 +1,3 @@
+to_do = ['Get quarters', 'Do laundry', 'Take a walk', 'Get a haircut', 'Make some tea', 'Compile Lists chapter', 'Call mom', 'Watch My hero Academia'] 
+print (to_do[0:2])
+print (to_do [4:7]) 
